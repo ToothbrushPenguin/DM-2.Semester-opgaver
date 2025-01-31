5ced6235a53650bf8c6e7a4a5038c9b43be2d26c
Archie <aromlov@gmail.com>
2025-01-31 11:24:01 +0100

    Added Film, and currently making film repo

---
+++ Pr38-40_TheMovies/TheMovies/TheMovies/Models/Film.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheMovies.Models
+{
+    public class Film
+    {
+        private static int idCount = 0;
+        private string title;
+        public string Title { get { return title; } }
+        private TimeSpan runTime;
+        public TimeSpan RunTime { get { return runTime; } set { runTime = value; } }
+        private string director;
+        public string Director { get { return director; } }
+        private double price;
+        public double Price { get { return price; } set { Price = value; } }
+        private int ageRestriction;
+        public int AgeRestriction { get { return ageRestriction; } }
+        private string genre;
+        public string Genre { get { return genre; } }
+
+
+        public Film(string title, TimeSpan runtime, string director, double price, int agerestriction, string genre)
+        {
+            idCount++;
+            this.title = title;
+            RunTime = runtime;
+            this.director = director;
+            Price = price;
+            this.ageRestriction = agerestriction;
+            this.genre = genre;
+        }
+    }
+}
---
+++ Pr38-40_TheMovies/TheMovies/TheMovies/Persistance/FilmRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheMovies.Models;
+
+namespace TheMovies.Persistance
+{
+    public class FilmRepository
+    {
+        private List<Film> films = new List<Film>();
+
+        public Film Create(string title, TimeSpan runtime, string director, double price, int agerestriction, string genre)
+        {
+            films.Add(new Film(title, runtime, director, price, agerestriction, genre));
+        }
+
+        public void Read()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Delete()
+        {
+
+        }
+    }
+}
