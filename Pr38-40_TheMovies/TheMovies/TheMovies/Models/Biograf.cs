b66e7523a34d756e60bcb1ee3d21dff7a11d7e3c
RayvenDK <dennis99@live.dk>
2025-01-31 10:51:04 +0100

    git test

--- Pr38-40_TheMovies/TheMovies/TheMovies/Models/Biograf.cs
+++ Pr38-40_TheMovies/TheMovies/TheMovies/Models/Biograf.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace TheMovies.Models
 {
-    internal class Biograf
+    public class Biograf
     {
+        private string _location;
+
+        public string Location
+        {
+            get { return _location; }
+            set { _location = value; }
+        }
+
+        private int _capacity;
+
+        public int Capacity
+        {
+            get { return _capacity; }
+            set { _capacity = value; }
+        }
+        // hej test 
     }
 }
